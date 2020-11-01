a84cbe10e8aa33fb626cdfab17f787c8e6348331
kiselyyovaalina <64138676+kiselyyovaalina@users.noreply.github.com>
2020-11-01 16:14:03 +0300

    Update main.cpp

--- 3 программа комбинаторика/main.cpp
+++ 3 программа комбинаторика/main.cpp
@@ -6,24 +6,31 @@ using namespace std;
 int main()
 {
     setlocale(LC_ALL, "rus");
+    int k=0,m=0;
     ofstream file;
     fstream text;
     text.open("INPUT.txt");
     file.open("OUTPUT.txt");
-    char a;
-    char b;
-    text>>a;
-    text>>b;
-    int sum=0;
-    while(a!=b)
+
+    string word;
+    text>>word;
+
+    for (int i=0;i<word.size();i++)
     {
-        sum++;
-        a=b;
-        text>>b;
+        if (word[i]!=word[i-1])
+        {
+            k++;
+            m = max(k,m);
+        }
+        else
+            {
+                k=0;
+            }
     }
-    file<<sum+1;
+    file<<m;
     cout<<"посмотрите результат в файле OUTPUT"<<endl;
     text.close();
     file.close();
     return 0;
 }
+
